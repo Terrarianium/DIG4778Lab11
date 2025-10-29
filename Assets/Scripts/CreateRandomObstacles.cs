4cea91b6ecb222c5cd209727f89f8abf864f88de
Terrarianium <Collinrogers915@gmail.com>
2025-10-29 14:34:39 -0400

    Random Grid

--- Assets/Scripts/CreateRandomObstacles.cs
+++ Assets/Scripts/CreateRandomObstacles.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class CreateRandomObstacles : MonoBehaviour
+public class CreateRandomObstacles
 {
-    // Start is called before the first frame update
-    void Start()
+    public static void GenerateRandomGrid(int width, int height, float obstacleProbability)
     {
-        
-    }
+        // Get the grid from the Pathfinding component on Maze object.
+        Pathfinding pathfinding = GameObject.Find("Maze").GetComponent<Pathfinding>();
+        pathfinding.grid = new int[width, height];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Initialize random number generator.
+        System.Random rand = new System.Random();
+
+        // Go through each cell in the grid.
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                // Set cell to obstacle based on probability.
+                if (rand.NextDouble() < obstacleProbability / 100f)
+                {
+                    pathfinding.grid[y, x] = 1;
+                }
+                else
+                {
+                    pathfinding.grid[y, x] = 0;
+                }
+            }
+        }
+
+        // Set start cell to 0.
+        pathfinding.grid[pathfinding.start.x, pathfinding.start.y] = 0;
+
+        // Set end cell to 0.
+        pathfinding.grid[pathfinding.goal.x, pathfinding.goal.y] = 0;
+
     }
 }
